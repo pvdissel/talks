f7af50992f76c2b9320f50729b458e08ad3ea33a
Patrick van Dissel <pvdissel@gmail.com>
2015-02-27 01:33:23 +0100

    Initial setup of Gradle-based, multi-project, Asciidoctor build, presentations

---
+++ subprojects/2015-spockframework-bol.com/src/test/groovy/com/bol/spock/sample/AwesomeSpec.groovy
@@ -0,0 +1,10 @@
+package com.bol.spock.sample
+
+import spock.lang.Specification
+
+class AwesomeSpec extends Specification {
+    def "Spock Framework is awesome"() {
+        expect:
+        true
+    }
+}
